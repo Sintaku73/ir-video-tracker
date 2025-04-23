ed4c309a86d8f81963f8daf2f81c3c9ba470c371
Sintaku <79888785+Sintaku73@users.noreply.github.com>
2025-04-23 23:37:07 +0900

    Implementation of yaw angle extraction using image rotation angle recognition

--- ir_video_tracker.m
+++ ir_video_tracker.m
@@ -2,7 +2,7 @@ clear
 close all
 
 %%
-v = VideoReader("input/2024-11-03 00-20-27_0_0.mp4");
+v = VideoReader("input/iRacing.com Simulator 2025-04-21 00-14-39.mp4");
 
 hImg = v.Height;
 wImg = v.Width;
@@ -13,15 +13,16 @@ hIdxStart = (hImg - hTrim)/2 + 1;
 wIdxStart = (wImg - wTrim)/2 + 1;
 
 %%
-iFrameStart = 44;
-iFrameEnd = 5854;
+iFrameStart = 185;
+iFrameEnd = 6230;
 intervalFrame = 10;
 listFrame = iFrameStart:intervalFrame:iFrameEnd;
 nFrame = length(listFrame);
 frameCurrent = read(v, iFrameStart);
 trimmedCurrent = frameCurrent(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
 grayCurrent = rgb2gray(trimmedCurrent);
-diffPixel = zeros(nFrame, 2);
+diffTranslation = zeros(nFrame, 2);
+diffAngle = zeros(nFrame,1);
 
 %%
 for i=progress(2:nFrame, "UpdateRate", 2)
@@ -29,27 +30,41 @@ for i=progress(2:nFrame, "UpdateRate", 2)
     trimmedNext = frameNext(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
     grayNext = rgb2gray(trimmedNext);
 
-    diffPixel(i, :) = getTranslation(grayCurrent,grayNext);
+    tform = getImgMove(grayCurrent,grayNext);
+    diffTranslation(i,:) = tform.Translation;
+    diffAngle(i) = tform.RotationAngle;
 
     grayCurrent = grayNext;
 end
-carPos = cumsum(diffPixel) .* [1 -1];
+% carPos = cumsum(diffTranslation).*[1 -1];
+cumAngle = cumsum(diffAngle);
+
+%% 
+rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
+
+diffRotated=zeros(size(diffTranslation));
+for i=2:nFrame
+    diffRotated(i,:)=transpose(rot(cumAngle(i-1))*diffTranslation(i,:).');
+end
+
+%% 
+carPos = cumsum(diffRotated).*[1 -1];
 
 %% Fix the defference between start and end
-frameStart = read(v,listFrame(1));
-frameEnd = read(v,listFrame(end));
-
-trimmedStart = frameStart(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
-trimmedEnd = frameEnd(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
-
-grayStart = rgb2gray(trimmedStart);
-grayEnd = rgb2gray(trimmedEnd);
-
-diffS2E = getTranslation(grayStart,grayEnd) .* [1 -1];
+% frameStart = read(v,listFrame(1));
+% frameEnd = read(v,listFrame(end));
+% 
+% trimmedStart = frameStart(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
+% trimmedEnd = frameEnd(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
+% 
+% grayStart = rgb2gray(trimmedStart);
+% grayEnd = rgb2gray(trimmedEnd);
+% 
+% diffS2E = getImgMove(grayStart,grayEnd).Translation.*[1 -1];
 
 %%
-shiftEnd = carPos(1,:)-carPos(end,:)+diffS2E;
-carPos = carPos+linspace(0,1,nFrame).'.*shiftEnd;
+% shiftEnd = carPos(1,:)-carPos(end,:)+diffS2E;
+% carPos = carPos+linspace(0,1,nFrame).'.*shiftEnd;
 
 %%
 figure
@@ -60,7 +75,7 @@ axis equal
 % save(sprintf("input/line_%dHz.mat",v.FrameRate/intervalFrame),"carPos","listFrame");
 
 %%
-function translation = getTranslation(gray1,gray2)
+function tform = getImgMove(gray1,gray2)
 pts1 = detectSURFFeatures(gray1);
 pts2 = detectSURFFeatures(gray2);
 
@@ -73,6 +88,4 @@ matched1 = validPts1(indexPairs(:,1));
 matched2 = validPts2(indexPairs(:,2));
 
 [tform,~] = estgeotform2d(matched2,matched1,"similarity");
-
-translation = tform.Translation;
 end
