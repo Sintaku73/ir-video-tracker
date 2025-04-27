07d9c3a90fa14f27b37dc450ec8cf4503c907bae
Sintaku <79888785+Sintaku73@users.noreply.github.com>
2025-04-27 16:45:42 +0900

    Remove unused functions: getCarMove, getImgMove, and trimImg

--- utils/getCarMove.m
+++
@@ -1,4 +0,0 @@
-function translation = getCarMove(tform,carCoG)
-rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-translation = tform.Translation-carCoG+transpose(rot(tform.RotationAngle)*carCoG.');
-end
