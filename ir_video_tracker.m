30488e2ed436662c1e48a7292e127acda8882924
Sintaku <79888785+Sintaku73@users.noreply.github.com>
2025-04-27 18:47:52 +0900

    Refactor car position and yaw calculations: consolidate logic into irvtUtils.getCarPos method

--- ir_video_tracker.m
+++ ir_video_tracker.m
@@ -6,73 +6,14 @@ addpath(fullfile(pwd,"utils"));
 
 v = VideoReader("input/iRacing.com Simulator 2025-04-21 00-14-39.mp4");
 
-hImg = v.Height;
-wImg = v.Width;
 hTrim = 720;
 wTrim = 1790;
-
-carCoG = [wTrim/2+0.5 hTrim/2+0.5];
-
-hIdxStart = (hImg - hTrim)/2 + 1;
-wIdxStart = (wImg - wTrim)/2 + 1;
-
-%%
 iFrameStart = 185;
 iFrameEnd = 6230;
 intervalFrame = 1;
-listFrame = iFrameStart:intervalFrame:iFrameEnd;
-nFrame = length(listFrame);
-frameCurrent = read(v, iFrameStart);
-trimmedCurrent = irvtUtils.trimImg(frameCurrent,hTrim,wTrim);
-grayCurrent = rgb2gray(trimmedCurrent);
-diffTranslation = zeros(nFrame, 2);
-diffAngle = zeros(nFrame,1);
 
 %%
-for i=progress(2:nFrame, "UpdateRate", 2)
-    frameNext = read(v, listFrame(i));
-    trimmedNext = irvtUtils.trimImg(frameNext,hTrim,wTrim);
-    grayNext = rgb2gray(trimmedNext);
-
-    tform = irvtUtils.getImgMove(grayCurrent,grayNext);
-    diffTranslation(i,:) = tform.Translation;
-    diffAngle(i) = tform.RotationAngle;
-
-    grayCurrent = grayNext;
-end
-
-%%
-rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-
-carYaw = cumsum(diffAngle);
-diffRotated=zeros(size(diffTranslation));
-for i=2:nFrame
-    diffRotated(i,:)=transpose(rot(carYaw(i-1))*diffTranslation(i,:).' ...
-        -rot(carYaw(i-1))*carCoG.'+rot(carYaw(i))*carCoG.');
-end
-
-%%
-carPos = cumsum(diffRotated);
-
-%% Fix the defference between start and end
-frameStart = read(v,listFrame(1));
-frameEnd = read(v,listFrame(end));
-
-trimmedStart = irvtUtils.trimImg(frameStart,hTrim,wTrim);
-trimmedEnd = irvtUtils.trimImg(frameEnd,hTrim,wTrim);
-
-grayStart = rgb2gray(trimmedStart);
-grayEnd = rgb2gray(trimmedEnd);
-
-tform = irvtUtils.getImgMove(grayStart,grayEnd);
-diffS2E = irvtUtils.getCarMove(tform,carCoG);
-
-%%
-shiftPos = carPos(1,:)-carPos(end,:)+diffS2E;
-carPos = (carPos+linspace(0,1,nFrame).'.*shiftPos).*[1 -1];
-
-shiftAngle = tform.RotationAngle-carYaw(end);
-carYaw = carYaw+linspace(0,1,nFrame).'.*shiftAngle;
+[carPos,carYaw,listFrame] = irvtUtils.getCarPos(v,hTrim,wTrim,iFrameStart,iFrameEnd,30);
 
 %%
 str = num2cell(listFrame);
--- utils/irvtUtils.m
+++ utils/irvtUtils.m
@@ -5,9 +5,12 @@ classdef irvtUtils
     end
 
     methods(Static)
+        function arrayRotated = rot(theta)
+            arrayRotated = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
+        end
+
         function translation = getCarMove(tform,carCoG)
-            rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-            translation = tform.Translation-carCoG+transpose(rot(tform.RotationAngle)*carCoG.');
+            translation = tform.Translation-carCoG+transpose(irvtUtils.rot(tform.RotationAngle)*carCoG.');
         end
 
         function tform = getImgMove(gray1,gray2)
@@ -28,18 +31,67 @@ classdef irvtUtils
         function imgTrimmed = trimImg(imgOrig,hTrim,wTrim)
             hImg = size(imgOrig,1);
             wImg = size(imgOrig,2);
-            hIdxStart = (hImg - hTrim)/2 + 1;
-            wIdxStart = (wImg - wTrim)/2 + 1;
-            imgTrimmed = imgOrig(hIdxStart:hIdxStart+hTrim-1, wIdxStart:wIdxStart+wTrim-1, :);
+            hIdxStart = (hImg-hTrim)/2+1;
+            wIdxStart = (wImg-wTrim)/2+1;
+            imgTrimmed = imgOrig(hIdxStart:hIdxStart+hTrim-1,wIdxStart:wIdxStart+wTrim-1,:);
         end
 
         function imgHollow = deleteAroundCar(imgOrig,hCut,wCut)
             hImg = size(imgOrig,1);
             wImg = size(imgOrig,2);
-            hIdxStart = (hImg - hCut)/2 + 1;
-            wIdxStart = (wImg - wCut)/2 + 1;
-            imgOrig(hIdxStart:hIdxStart+hCut-1, wIdxStart:wIdxStart+wCut-1, :) = 0;
+            hIdxStart = (hImg-hCut)/2+1;
+            wIdxStart = (wImg-wCut)/2+1;
+            imgOrig(hIdxStart:hIdxStart+hCut-1,wIdxStart:wIdxStart+wCut-1,:) = 0;
             imgHollow = imgOrig;
+        end
+
+        function [carPos,carYaw,listFrame] = getCarPos(v,hTrim,wTrim,iFrameStart,iFrameEnd,intervalFrame)
+            listFrame = iFrameStart:intervalFrame:iFrameEnd;
+            nFrame = length(listFrame);
+            frameCurrent = read(v,iFrameStart);
+            trimmedCurrent = irvtUtils.trimImg(frameCurrent,hTrim,wTrim);
+            grayCurrent = rgb2gray(trimmedCurrent);
+            diffTranslation = zeros(nFrame,2);
+            diffAngle = zeros(nFrame,1);
+
+            for i=progress(2:nFrame,"UpdateRate",2)
+                frameNext = read(v,listFrame(i));
+                trimmedNext = irvtUtils.trimImg(frameNext,hTrim,wTrim);
+                grayNext = rgb2gray(trimmedNext);
+
+                tform = irvtUtils.getImgMove(grayCurrent,grayNext);
+                diffTranslation(i,:) = tform.Translation;
+                diffAngle(i) = tform.RotationAngle;
+
+                grayCurrent = grayNext;
+            end
+
+            carCoG = [wTrim/2+0.5 hTrim/2+0.5];
+            carYaw = cumsum(diffAngle);
+            diffRotated=zeros(size(diffTranslation));
+            for i=2:nFrame
+                diffRotated(i,:)=transpose(irvtUtils.rot(carYaw(i-1))*diffTranslation(i,:).' ...
+                    -irvtUtils.rot(carYaw(i-1))*carCoG.'+irvtUtils.rot(carYaw(i))*carCoG.');
+            end
+            carPos = cumsum(diffRotated);
+
+            % Fix the difference between start and end
+            frameStart = read(v,listFrame(1));
+            frameEnd = read(v,listFrame(end));
+
+            trimmedStart = irvtUtils.trimImg(frameStart,hTrim,wTrim);
+            trimmedEnd = irvtUtils.trimImg(frameEnd,hTrim,wTrim);
+            grayStart = rgb2gray(trimmedStart);
+            grayEnd = rgb2gray(trimmedEnd);
+
+            tform = irvtUtils.getImgMove(grayStart,grayEnd);
+            diffS2E = irvtUtils.getCarMove(tform,carCoG);
+
+            shiftPos = carPos(1,:)-carPos(end,:)+diffS2E;
+            carPos = (carPos+linspace(0,1,nFrame).'.*shiftPos).*[1 -1];
+
+            shiftAngle = tform.RotationAngle-carYaw(end);
+            carYaw = carYaw+linspace(0,1,nFrame).'.*shiftAngle;
         end
     end
 end
