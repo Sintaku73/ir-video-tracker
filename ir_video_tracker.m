53bbca3b8caf85625053893f4d3a837ebc374ea6
Sintaku <79888785+Sintaku73@users.noreply.github.com>
2025-04-28 17:33:28 +0900

    Fix formatting in for loop declarations for consistency

--- ir_video_tracker.m
+++ ir_video_tracker.m
@@ -29,7 +29,7 @@ diffTranslation = zeros(nFrame, 2);
 diffAngle = zeros(nFrame,1);
 
 %%
-for i=progress(2:nFrame, "UpdateRate", 2)
+for i = progress(2:nFrame, "UpdateRate", 2)
     frameNext = read(v, listFrame(i));
     trimmedNext = irvtUtils.trimImg(frameNext,hTrim,wTrim);
     grayNext = rgb2gray(trimmedNext);
@@ -46,7 +46,7 @@ rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
 
 cumAngle = cumsum(diffAngle);
 diffRotated=zeros(size(diffTranslation));
-for i=2:nFrame
+for i = 2:nFrame
     diffRotated(i,:)=transpose(rot(cumAngle(i-1))*diffTranslation(i,:).' ...
         -rot(cumAngle(i-1))*carCoG.'+rot(cumAngle(i))*carCoG.');
 end
--- ir_video_tracker_telem.m
+++ ir_video_tracker_telem.m
@@ -84,7 +84,7 @@ diffTranslation = zeros(nFrame, 2);
 diffAngle = zeros(nFrame,1);
 
 %%
-for i=progress(2:nFrame, "UpdateRate", 2)
+for i = progress(2:nFrame, "UpdateRate", 2)
     frameNext = read(v, listFrame(i));
     trimmedNext = irvtUtils.trimImg(frameNext,hTrim,wTrim);
     grayNext = rgb2gray(trimmedNext);
@@ -101,7 +101,7 @@ rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
 
 cumAngle = cumsum(diffAngle);
 diffRotated=zeros(size(diffTranslation));
-for i=2:nFrame
+for i = 2:nFrame
     diffRotated(i,:)=transpose(rot(cumAngle(i-1))*diffTranslation(i,:).' ...
         -rot(cumAngle(i-1))*carCoG.'+rot(cumAngle(i))*carCoG.');
 end
