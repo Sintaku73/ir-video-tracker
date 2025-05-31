1364a698f114988dd4526017f33174a186e077b9
Sintaku <79888785+Sintaku73@users.noreply.github.com>
2025-05-31 21:20:11 +0900

    Update README and clean up import_logdata.m; enhance documentation and remove unused code

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
 # iR Video Tracker
 
+このツールを使うことでiRacingのリプレイ映像から走行データ(走行ライン、速度、ヨー角など)を取得できます。  
+次のような活用法が期待されます。
 
+- telemetryを取り忘れたラップの分析をしたい。
+- 手元にtelemetryデータのないライバルの走りを分析したい。
 
 ## Getting Started
 
-
-
 ### Prerequisites
 
-
+- MATLAB
+  - Computer Vision Toolbox
+  - Image Processing Toolbox
+  - MatlabProgressBar
+  - matmap3d
 
 ### Installing
 
+```bash
+git clone https://github.com/Sintaku73/ir-video-tracker.git
+```
 
+## Usage and Examples
 
-## Running the tests
-
-
-
-### Break down into end to end tests
-
-
-
-### And coding style tests
-
-
+```mermaid
+flowchart
+s([start])
+e([end])
+motivation["利用目的<br>1. インタラクティブな走行ラインの確認<br>2. "]
+```
 
 ## Deployment
 
--- test/import_logdata.m
+++ test/import_logdata.m
@@ -14,11 +14,6 @@ samplingRate = 1/mean(diff(Lap.Time));
 lapN = Lap.Value./max(Lap.Value);
 distN = Lap_Distance.Value./max(Lap_Distance.Value);
 
-% figure
-% area(distN,"FaceAlpha",0.5)
-% hold on
-% area(lapN,"FaceAlpha",0.5)
-
 lapSelected = 3;
 idxValid = Lap.Value==lapSelected;
 
@@ -56,87 +51,3 @@ hold on
 quiver(x,y,vectorYaw(1,:),vectorYaw(2,:),"off")
 scatter(x,y,5,rad2deg(yawValid))
 scatter(x(idxJump),y(idxJump),"red")
-
-%%
-
-
-%%
-v = VideoReader("input/iRacing.com Simulator 2025-04-21 00-14-39.mp4");
-
-hImg = v.Height;
-wImg = v.Width;
-hTrim = 720;
-wTrim = 1790;
-
-carCoG = [wTrim/2+0.5 hTrim/2+0.5];
-
-hIdxStart = (hImg - hTrim)/2 + 1;
-wIdxStart = (wImg - wTrim)/2 + 1;
-
-%%
-iFrameEnd = 6230;
-intervalFrame = 1;
-nFrame = length(listFrame);
-frameCurrent = read(v, iFrameStart);
-trimmedCurrent = irvtUtils.trimImg(frameCurrent,hTrim,wTrim);
-grayCurrent = rgb2gray(trimmedCurrent);
-diffTranslation = zeros(nFrame, 2);
-diffAngle = zeros(nFrame,1);
-
-%%
-for i = progress(2:nFrame, "UpdateRate", 2)
-    frameNext = read(v, listFrame(i));
-    trimmedNext = irvtUtils.trimImg(frameNext,hTrim,wTrim);
-    grayNext = rgb2gray(trimmedNext);
-
-    tform = irvtUtils.getImgMove(grayCurrent,grayNext);
-    diffTranslation(i,:) = tform.Translation;
-    diffAngle(i) = tform.RotationAngle;
-
-    grayCurrent = grayNext;
-end
-
-%%
-rot = @(theta) [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-
-carYaw = cumsum(diffAngle);
-diffRotated=zeros(size(diffTranslation));
-for i = 2:nFrame
-    diffRotated(i,:)=transpose(rot(carYaw(i-1))*diffTranslation(i,:).' ...
-        -rot(carYaw(i-1))*carCoG.'+rot(carYaw(i))*carCoG.');
-end
-
-%%
-carPos = cumsum(diffRotated);
-
-%% Fix the defference between start and end
-frameStart = read(v,listFrame(1));
-frameEnd = read(v,listFrame(end));
-
-trimmedStart = irvtUtils.trimImg(frameStart,hTrim,wTrim);
-trimmedEnd = irvtUtils.trimImg(frameEnd,hTrim,wTrim);
-
-grayStart = rgb2gray(trimmedStart);
-grayEnd = rgb2gray(trimmedEnd);
-
-tform = irvtUtils.getImgMove(grayStart,grayEnd);
-diffS2E = irvtUtils.getCarMove(tform,carCoG);
-
-%%
-shiftPos = carPos(1,:)-carPos(end,:)+diffS2E;
-carPos = (carPos+linspace(0,1,nFrame).'.*shiftPos).*[1 -1];
-
-shiftAngle = tform.RotationAngle-carYaw(end);
-carYaw = carYaw+linspace(0,1,nFrame).'.*shiftAngle;
-
-%%
-str = num2cell(listFrame);
-figure
-plot(carPos(:,1),carPos(:,2),".-")
-axis equal
-grid on
-% hold on
-% text(carPos(:,1),carPos(:,2),str)
-
-%%
-% save(sprintf("input/line_%dHz.mat",v.FrameRate/intervalFrame),"carPos","carYaw","listFrame");
